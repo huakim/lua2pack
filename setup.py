87f18ecb4b6c02a0fae8240e587689b0aa41a194
huakim <email>
2024-05-24 22:57:26 +0300

    Initial commit  Changes to be committed: 	new file:   .gitignore 	new file:   LICENSE 	new file:   MANIFEST.in 	new file:   conf.py 	new file:   lua2pack.py 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+import conf
+
+attrs={}
+
+for i in dir(conf):
+    if not i.startswith('_'):
+        attrs[i] = getattr(conf, i)
+
+setup(
+ py_modules=['lua2pack'],
+ install_requires=['jinja2_easy.generator', 'lupa'],
+ **attrs
+)
