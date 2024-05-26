312fda8564a4897cb288ffdc27f38c1fb95938a1
huakim <92579808+huakim@users.noreply.github.com>
2024-05-26 17:01:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ for i in dir(conf):
         attrs[i] = getattr(conf, i)
 
 setup(
+ entry_points = {
+    'console_scripts': [
+       'lua2pack = lua2pack:main',
+    ],
+ }
  py_modules=['lua2pack'],
  install_requires=['jinja2-easy.generator', 'lupa'],
  **attrs
