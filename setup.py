6523b3d573a8b129d6d288d934481fbd6268041c
huakim <92579808+huakim@users.noreply.github.com>
2024-05-26 17:25:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 setup(
  name="lua2pack",
  version="0.0.1",
- description = "Generate RPM spec files from luarocks"
- summary = "Generate RPM spec files from luarocks"
- license = "GPLv3"
- url = "https://github.com/huakim/lua2pack"
+ description = "Generate RPM spec files from luarocks",
+ summary = "Generate RPM spec files from luarocks",
+ license = "GPLv3",
+ url = "https://github.com/huakim/lua2pack",
  packages=find_packages(),
  entry_points = {
     'console_scripts': [
