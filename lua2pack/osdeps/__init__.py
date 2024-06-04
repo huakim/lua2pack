ca68c4078067ee9d7d9d1469afc2f72eefc174e9
u <u>
2024-06-04 22:47:36 +0300

    update

--- lua2pack/osdeps/__init__.py
+++ lua2pack/osdeps/__init__.py
@@ -5,6 +5,26 @@ def generate_args(parser):
     parser.add_argument("--arch", help="Architecture name")
     parser.add_argument('--name', help="Override default name")
 
+class LuaBool():
+    def __init__(self, a):
+        self.__a = bool(a)
+    def __bool__(self):
+        return self.__a
+    def __str__(self):
+        return 'true' if self else 'false'
+
+def is_enabled_array(array, default=False, revert=False):
+    if not array is None:
+        default = array[1] == 'enable' if len(array) > 0 else True
+        if revert:
+            default = not default
+    return LuaBool(default)
+
+def is_enabled_flag(arg, not_arg, default):
+    return is_enabled_array(not_arg if default else arg,
+        is_enabled_array(arg if default else not_arg,
+            default, default), not default)
+
 def lua_code(args):
     '''add lua code'''
     system = args.system or platform.system()
--- lua2pack/osdeps/opensuse.py
+++ lua2pack/osdeps/opensuse.py
@@ -1,11 +1,26 @@
-def lua_code(args):
-    return """
+from .. import is_enabled_array
 
-subpackages = true
-autogen = false
-filelist = true
-skip_build_dependencies = false
-skip_check_dependencies = false
+def generate_args(parser):
+    parser.add_argument('--subpackages', action='store', nargs='*')
+    parser.add_argument('--autogen', action='store', nargs='*')
+    parser.add_argument('--filelist', action='store', nargs='*')
+    parser.add_argument('--skip-build-dependencies', action='store', nargs='*')
+    parser.add_argument('--skip-check-dependencies', action='store', nargs='*')
+    parser.add_argument('--no-subpackages', action='store', nargs='*')
+    parser.add_argument('--no-autogen', action='store', nargs='*')
+    parser.add_argument('--no-filelist', action='store', nargs='*')
+    parser.add_argument('--no-skip-build-dependencies', action='store', nargs='*')
+    parser.add_argument('--no-skip-check-dependencies', action='store', nargs='*')
+
+
+def lua_code(args):
+    return f"""
+
+subpackages = {is_enabled_array(args.subpackages, args.no_subpackages, True)}
+autogen = {is_enabled_array(args.autogen, args.no_autogen, False)}
+filelist = {is_enabled_array(args.filelist, args.no_filelist, True)}
+skip_build_dependencies = {is_enabled_array(args.skip_build_dependencies, args.no_skip_build_dependencies, False)}
+skip_check_dependencies = {is_enabled_array(args.skip_check_dependencies, args.no_skip_check_dependencies, False)}
 template = 'generic.spec'
 
 """
