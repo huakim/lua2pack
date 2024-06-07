3949bf1c62441db19d189edf9da79064e48eaa6b
huakim <fijik19@gmail.com>
2024-06-07 19:25:26 +0000

    update

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
  name="lua2pack",
- version="0.0.3",
- description = "Generate RPM spec files from luarocks",
- summary = "Generate RPM spec files from luarocks",
+ version="0.0.5",
+ description = "Generate RPM or DSC spec files from luarocks",
+ summary = "This utility is used for generating files of specific template from luarocks",
  license = "GPLv3",
  url = "https://github.com/huakim/lua2pack",
  packages=find_packages(),
